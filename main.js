9ca4556998e4fcb82721ce0ccff4469d7681b15c
McLivraison <mclivraison123@gmail.com>
2022-03-22 08:39:56 +1000

    Added in the basic hello world code, or essentially just a ping-pong bot

---
+++ main.js
@@ -0,0 +1,25 @@
+// Using this tutorial: https://www.writebots.com/how-to-make-a-discord-bot/
+
+// Require the necessary discord.js classes
+import  { Client, Intents } from 'discord.js';
+import logger from 'winston';
+import auth from './auth.json' assert {type: "json"};
+import mypackage from './package.json' assert {type: "json"};
+
+logger.level = 'debug';
+
+// Initialize Discord Bot
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+
+client.once('ready', () => {
+    console.log('Ready!');
+});
+
+client.on('message', msg => {
+    console.log(`Message is: ${msg}`);
+    if (msg.content === 'ping') {
+      msg.channel.send('pong');
+    }
+  });
+
+client.login(auth.token);
